1dde7e1d8d434ce49d4aa9c84d25794a3c7da053
Align807 <66521673+Align807@users.noreply.github.com>
2021-12-03 21:10:40 +0530

    jobdata change

--- src/Data/JobData.js
+++ src/Data/JobData.js
@@ -3,14 +3,14 @@ export const jobData_withoutcomplete=[
     { "status": "0", "priority": "2", "description": "Drip Irrigation", "category": "1", "asset": "None", "duedate": "11/25/2021", "lastupdate": "11/24/2021" },
    
     { "status": "1", "priority": "3", "description": "Plough Field", "category": "0", "asset": "None", "duedate": "11/23/2021", "lastupdate": "11/26/2021" },
-    { "status": "3", "priority": "1", "description": "Manure and Fertilizer", "category": "2", "asset": "None", "duedate": "11/26/2021", "lastupdate": "11/27/2021" }
-  
+    { "status": "3", "priority": "1", "description": "Manure and Fertilizer", "category": "2", "asset": "None", "duedate": "11/26/2021", "lastupdate": "11/27/2021" },
+    { "status": "2", "priority": "4", "description": "Drip Irrigation", "category": "1", "asset": "None", "duedate": "11/25/2021", "lastupdate": "11/24/2021" },
   ];
   
   export const jobData_withcomplete=[
     { "status": "0", "priority": "2", "description": "Drip Irrigation", "category": "1", "asset": "None", "duedate": "11/25/2021", "lastupdate": "11/24/2021" },
     { "status": "4", "priority": "0", "description": "Paddy field managing", "category": "4", "asset": "None", "duedate": "11/23/2021", "lastupdate": "11/25/2021" },
     { "status": "1", "priority": "3", "description": "Plough Field", "category": "0", "asset": "None", "duedate": "11/23/2021", "lastupdate": "11/26/2021" },
-    { "status": "3", "priority": "1", "description": "Manure and Fertilizer", "category": "2", "asset": "None", "duedate": "11/26/2021", "lastupdate": "11/27/2021" }
-  
+    { "status": "3", "priority": "1", "description": "Manure and Fertilizer", "category": "2", "asset": "None", "duedate": "11/26/2021", "lastupdate": "11/27/2021" },
+    { "status": "2", "priority": "4", "description": "Drip Irrigation", "category": "1", "asset": "None", "duedate": "11/25/2021", "lastupdate": "11/24/2021" },
   ];
